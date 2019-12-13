ad1dfbef2e0c5c80d51c3c6ec75689afed00ef86
Mark-Ball <markball111@gmail.com>
2019-12-14 10:19:36 +1100

    test for /pokemon endpoint working

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/pokemon', (req, res) => { res.json({message: 'hi' }) });
+router.get('/pokemon', (req, res) => { res.json({ pokemon: 'pikachu' }) });
 
 module.exports = router;
--- tests/integration/pokemon/index.test.js
+++ tests/integration/pokemon/index.test.js
@@ -1,18 +1,18 @@
+// const request = require('supertest');
+// const app = require('./../../../app');
+// require('./../../setup');
+
+// test("index returns status", async () => {
+//     const response = await request(app).get("/pokemon").expect(200);
+// });
+
 const request = require('supertest');
 const app = require('./../../../app');
-require('./../../setup');
 
-describe("GET pokemon index", () => {
-    test("index returns status", () => {
-        const response = await request(app).get("/pokemon").expect(200);
-    });
-});
+test("Check jest is working", () => {
+    expect(1).toBe(1);
+})
 
-// test("this one should be successful", () => {
-//     expect(1).toBe(1);
-// })
-
-// test("testing to see if jest works", () => {
-//     expect(1).toBe(2);
-// })
-
+test("GET at /pokemon", () => {
+    const response = request(app).get('/pokemon').expect(200);
+})
