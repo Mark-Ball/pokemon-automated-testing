fa4fa45a3c239be55cf35642fa66c3332edbd147
Mark-Ball <markball111@gmail.com>
2019-12-15 09:44:51 +1100

    wrote tests for index using axios

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 router.get('/pokemon', (req, res) => { res.json({ pokemon: 'pikachu' }) });
 
--- tests/integration/pokemon/index.test.js
+++ tests/integration/pokemon/index.test.js
@@ -1,18 +1,39 @@
-// const request = require('supertest');
-// const app = require('./../../../app');
-// require('./../../setup');
-
-// test("index returns status", async () => {
-//     const response = await request(app).get("/pokemon").expect(200);
-// });
-
 const request = require('supertest');
 const app = require('./../../../app');
+const axios = require('axios');
+
+test("Axios returns 200 status", async () => {
+    const response = await axios.get('http://localhost:3000/pokemon');
+    expect(response.status).toBe(200);
+});
+
+test("Axios returns pokemon object", async () => {
+    const response = await axios.get('http://localhost:3000/pokemon');
+    expect(response.data).toMatchObject({ 'pokemon': /./ })
+})
 
 test("Check jest is working", () => {
     expect(1).toBe(1);
-})
+});
 
-test("GET at /pokemon", () => {
-    const response = request(app).get('/pokemon').expect(200);
-})
+// // passes, but Jest does not exit after test complete
+// test("GET at /pokemon returns 200 status", function() {
+//     const response = request(app).get('/pokemon').expect(200);
+// })
+
+// app.get('/user', function (req, res) {
+//     res.status(200).json({ name: 'John' });
+// });
+
+// request(app)
+//     .get('/user')
+//     .expect(200)
+//     .end(function(err, res) {
+//         if (err) throw err;
+//     });
+
+// test('responds with json', function(done) {
+//     request(app)
+//         .get('/user')
+//         .expect(200, done);
+// });
