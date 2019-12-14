a11fab184883768a52c65cee844aae985cf4e5f8
Mark-Ball <markball111@gmail.com>
2019-12-15 09:46:02 +1100

    removed unnecessary axios require in routes

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
 
 router.get('/pokemon', (req, res) => { res.json({ pokemon: 'pikachu' }) });
 
